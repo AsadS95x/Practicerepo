22ac7bbcb700e546c4f26d00a3b81b64e6423025
Asad <AsadS95@live.co.uk>
2022-05-16 15:53:55 +0100

    First Update on python file

--- ehh.py
+++ ehh.py
@@ -1,5 +1,9 @@
-def ehh{
-
-}
+def ehh():
+    return null
 
 print ("Hi i don't know you" )
+fname = input ("Whats your name? :")
+sname = input ("And your surname too :" )
+
+print ("You have told me your name is " + fname + " " +  sname)
+print ("is this correct? ")
