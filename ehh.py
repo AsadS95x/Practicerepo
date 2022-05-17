0333a10f4c2ac9aad37d50dc139c631c1947b944
Asad <AsadS95@live.co.uk>
2022-05-17 11:05:39 +0100

    First Update on python file

--- ehh.py
+++ ehh.py
@@ -1,5 +1,18 @@
-def ehh{
+def ehh(reply):
+    if reply.upper() == 'Y':
+        print ( "Wohooo!")
+    elif reply.upper() == 'N' :
+        print ("Goddaaamit!") 
+        op()
+    
 
-}
+def op():
+    print ("Hi i don't know you" )
+    fname = input ("Whats your name? :")
+    sname = input ("And your surname too :" )
+    print ("You have told me your name is " + fname + " " +  sname)
+    print (ehh(input ("Is this correct? Y/N ")))
 
-print ("Hi i don't know you" )
+op()
+
+mann fuck this
