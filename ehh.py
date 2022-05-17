e79a233c0ca402b78861aacc153d21714a75e1c5
Asad <AsadS95@live.co.uk>
2022-05-17 11:23:07 +0100

    Py Function loops

--- ehh.py
+++ ehh.py
@@ -13,6 +13,31 @@ def op():
     print ("You have told me your name is " + fname + " " +  sname)
     print (ehh(input ("Is this correct? Y/N ")))
 
-op()
+def dev_money():
+    if devs_money > 10 and dev_can_play_smash:
+        print("Dev enters a smash tournament!")
+    elif devs_money < 10 and dev_can_play_smash:
+      print("Dev is too poor to enter")
+    else:
+     print("Dev just can't play smash")
 
-mann fuck this
+def class_score():
+    mark = int(input ("What was your mark?" ))
+    if mark >= 85: 
+        print ("Distinction")
+    elif mark <65:
+        print ("Fail")
+    else:
+        print ("Pass!")
+
+def studentnames():
+    count = 0
+    namelist = []
+    while count < 5:
+        name = str(input("What is the students name "))
+        namelist.append(name)
+        print("Student ", namelist[count], " is awesome!")
+        count += 1
+    print ("Students ",*namelist(), " are all awesome! ")
+
+studentnames()
